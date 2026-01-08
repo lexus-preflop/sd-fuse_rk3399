c3edb6d9638eb0898a08131efd1ba8f01269db16
preflop <bpseditor1@outlook.com>
2026-01-08 22:19:54 +0300

    Refactor kernel and driver download paths in script
    Updated paths for downloading and extracting kernel and drivers.

--- test/test-update-kernel-for-debiancore.sh
+++ test/test-update-kernel-for-debiancore.sh
@@ -19,21 +19,22 @@ cd sd-fuse_rk3399
 if [ -f ../../debian-bookworm-core-arm64-images.tgz ]; then
 	tar xvzf ../../debian-bookworm-core-arm64-images.tgz
 else
-	wget --no-proxy http://${HTTP_SERVER}/dvdfiles/RK3399/images-for-eflasher/debian-bookworm-core-arm64-images.tgz
-    tar xvzf debian-bookworm-core-arm64-images.tgz
+	wget --no-proxy -P ../../  http://${HTTP_SERVER}/dvdfiles/RK3399/images-for-eflasher/debian-bookworm-core-arm64-images.tgz
+    tar xvzf ../../debian-bookworm-core-arm64-images.tgz
 fi
 
-if [ -f ../../kernel-rk3399.tgz ]; then
-	tar xvzf ../../kernel-rk3399.tgz
+if [ -f ../../kernel-rk3399 ]; then
+	cp -rf ../../kernel-rk3399 .
 else
-	git clone ${KERNEL_URL} --depth 1 -b ${KERNEL_BRANCH} kernel-rk3399
+	git clone ${KERNEL_URL} --depth 1 -b ${KERNEL_BRANCH} ../../kernel-rk3399
+    cp -rf ../../kernel-rk3399 .
 fi
 
-wget http://${HTTP_SERVER}/sd-fuse/kernel-3rd-drivers.tgz
-if [ -f kernel-3rd-drivers.tgz ]; then
-    pushd out
-    tar xzf ../kernel-3rd-drivers.tgz
-    popd
+if [ -f ../../kernel-3rd-drivers.tgz ]; then
+    tar xzf ../../kernel-3rd-drivers.tgz
+else
+    wget -P ../../ http://${HTTP_SERVER}/sd-fuse/kernel-3rd-drivers.tgz
+    tar xzf ../../kernel-3rd-drivers.tgz
 fi
 
 KERNEL_SRC=$PWD/kernel-rk3399 ./build-kernel.sh debian-bookworm-core-arm64
